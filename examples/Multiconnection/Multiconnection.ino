42cd0d2ccc6221ab07d88b41cd73903502ffb2a4
ELEMYO <kovalev.oleg.o@gmail.com>
2020-04-22 16:13:55 +0300

    Update Multiconnection.ino

--- examples/Multiconnection/Multiconnection.ino
+++ examples/Multiconnection/Multiconnection.ino
@@ -3,6 +3,7 @@
 //
 // Changelog:
 //     2020-02-24 - initial release
+//     2020-04-22 - BandStop filter added
 
 /* ============================================
 ELEMYO library code is placed under the MIT license
@@ -33,8 +34,8 @@ Wiring the ELEMYO sensor to an Arduino
 ELEMYO -->  Arduino
   +        5V
   -        GND
-  S        analog pin
-  СS       diginal pin (SS pin)
+  S        analog pin A0 (or other analog input)
+  СS       pin 10 (SS pin)
   MOSI     pin 11 (UNO, Nano, Mini), pin 51 (Mega), pin SI (Leonardo), pin 16 (Pro micro)
   SCK      pin 13 (UNO, Nano, Mini), pin 52 (Mega), pin SCK (Leonardo), pin 15 (Pro micro)
 
@@ -57,7 +58,12 @@ ELEMYO -->  Arduino
 #define   sensorInPin5   A4     
 #define   sensorInPin6   A5     
 
-#define   timePeriod    1      // Frequency of signal update (time in ms)
+int sensorValue1 = 0;           // value read from the sensor 1
+int sensorValue2 = 0;           // value read from the sensor 2
+int sensorValue3 = 0;           // value read from the sensor 3
+int sensorValue4 = 0;           // value read from the sensor 4
+int sensorValue5 = 0;           // value read from the sensor 5
+int sensorValue6 = 0;           // value read from the sensor 6
 
 // sensors initialization:
 ELEMYO MyoSensor1(CSpin1);
@@ -92,19 +98,42 @@ void setup() {
 
 void loop() {
 
-  // printing the results to the Serial Monitor:
-  Serial.print(analogRead(sensorInPin1));
+  // read the analog in value:
+  sensorValue1 = analogRead(sensorInPin1);
+  sensorValue2 = analogRead(sensorInPin2);
+  sensorValue3 = analogRead(sensorInPin3);
+  sensorValue4 = analogRead(sensorInPin4);
+  sensorValue5 = analogRead(sensorInPin5);
+  sensorValue6 = analogRead(sensorInPin6);
+  
+  // notch 50 Hz filter with band window 4 Hz
+  sensorValue1 = MyoSensor1.BandStop(sensorValue1, 50, 4);
+  sensorValue2 = MyoSensor2.BandStop(sensorValue2, 50, 4);
+  sensorValue3 = MyoSensor3.BandStop(sensorValue3, 50, 4);
+  sensorValue4 = MyoSensor4.BandStop(sensorValue4, 50, 4);
+  sensorValue5 = MyoSensor5.BandStop(sensorValue5, 50, 4);
+  sensorValue6 = MyoSensor6.BandStop(sensorValue6, 50, 4);
+
+  // notch 100 Hz (one of 50 Hz mode) filter with band window 6 Hz
+  sensorValue1 = MyoSensor1.BandStop(sensorValue1, 100, 6);
+  sensorValue2 = MyoSensor2.BandStop(sensorValue2, 100, 6);
+  sensorValue3 = MyoSensor3.BandStop(sensorValue3, 100, 6);
+  sensorValue4 = MyoSensor4.BandStop(sensorValue4, 100, 6);
+  sensorValue5 = MyoSensor5.BandStop(sensorValue5, 100, 6);
+  sensorValue6 = MyoSensor6.BandStop(sensorValue6, 100, 6);
+  
+  Serial.print(sensorValue1+500);
   Serial.print(" ");
-  Serial.print(analogRead(sensorInPin2));
+  Serial.print(sensorValue2+1000);
   Serial.print(" ");
-  Serial.print(analogRead(sensorInPin3));
+  Serial.print(sensorValue3+1500);
   Serial.print(" ");
-  Serial.print(analogRead(sensorInPin4));
+  Serial.print(sensorValue4+2000);
   Serial.print(" ");
-  Serial.print(analogRead(sensorInPin5));
+  Serial.print(sensorValue5+2500);
   Serial.print(" ");
-  Serial.println(analogRead(sensorInPin6));
+  Serial.println(sensorValue6+3000);
 
   // wait before the next loop
-  delay(timePeriod);
+  delay(0);
 }
